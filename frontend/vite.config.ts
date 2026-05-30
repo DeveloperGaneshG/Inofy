9f1be7eaa1490604317ffa955ad807db7d7cda0a
sainani7799 <136177130+sainani7799@users.noreply.github.com>
2026-05-30 12:31:47 +0530

    Set base path for Vite configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-  server: { port: 5173 },
 });
